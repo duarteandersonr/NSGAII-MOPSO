53e70f8fe0d914b793a39e9f97cad30544985a41
duarteandersonr <120497714+duarteandersonr@users.noreply.github.com>
2022-12-13 15:03:43 -0300

    Add files via upload

---
+++ exec.bat
@@ -0,0 +1,19 @@
+gapsoMG1k.exe < SeNq3La5S05.txt >> output_SeNq3La5S05.txt
+gapsoMG1k.exe < SeNq3La5S10.txt >> output_SeNq3La5S10.txt
+gapsoMG1k.exe < SeNq3La5S15.txt >> output_SeNq3La5S15.txt
+
+gapsoMG1k.exe < MeNq3La5S05.txt >> output_MeNq3La5S05.txt
+gapsoMG1k.exe < MeNq3La5S10.txt >> output_MeNq3La5S10.txt
+gapsoMG1k.exe < MeNq3La5S15.txt >> output_MeNq3La5S15.txt
+
+gapsoMG1k.exe < SpNq3La5S05.txt >> output_SpNq3La5S05.txt
+gapsoMG1k.exe < SpNq3La5S10.txt >> output_SpNq3La5S10.txt
+gapsoMG1k.exe < SpNq3La5S15.txt >> output_SpNq3La5S15.txt
+
+gapsoMG1k.exe < MxNq6La5S05.txt >> output_MxNq6La5S05.txt
+gapsoMG1k.exe < MxNq6La5S10.txt >> output_MxNq6La5S10.txt
+gapsoMG1k.exe < MxNq6La5S15.txt >> output_MxNq6La5S15.txt
+
+
+
+
